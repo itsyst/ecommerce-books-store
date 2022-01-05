a8c394a378d7957353ffe6e565a0d73d6d716584
Khaled El Hamzi <contact@elhamzi.me>
2022-01-05 23:15:56 +0100

    Using code first approach.

---
+++ BooksWebApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BooksWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWebApp.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
