87a65c947f3302921c69c2234ec0be50f8dd6bce
Khaled El Hamzi <contact@elhamzi.me>
2022-01-05 23:25:12 +0100

    Use sql server service.

--- BooksWebApp/Program.cs
+++ BooksWebApp/Program.cs
@@ -1,7 +1,13 @@
+using BooksWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDBContext>(options =>options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
