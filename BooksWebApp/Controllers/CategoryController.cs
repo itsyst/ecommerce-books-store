7378318172fcb352797b696e16ec8e05fdb63731
Khaled El Hamzi <contact@elhamzi.me>
2022-01-06 00:24:17 +0100

    Retrieve and display data from the database.

--- BooksWebApp/Controllers/CategoryController.cs
+++ BooksWebApp/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BooksWebApp.Data;
+using BooksWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksWebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categoryList = _db.Categories;
+            return View(categoryList);
         }
     }
 }
