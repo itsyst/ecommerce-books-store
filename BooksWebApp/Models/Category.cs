abb61925f96edc3d54727382b5897b22dddfe197
Khaled El Hamzi <contact@elhamzi.me>
2022-01-05 22:24:15 +0100

    Add category model.

---
+++ BooksWebApp/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace BooksWebApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
