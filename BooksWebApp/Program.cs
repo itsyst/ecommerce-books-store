c7fc62ca6943a1b917e9cb7d63b78b948714976b
Khaled El Hamzi <contact@elhamzi.me>
2022-01-06 22:25:29 +0100

    Use bootswatch custom theme.

--- BooksWebApp/Program.cs
+++ BooksWebApp/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
