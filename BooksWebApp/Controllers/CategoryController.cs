a25c010f1420e0d08f260ba62de3d0e48deac8f7
Khaled El Hamzi <contact@elhamzi.me>
2022-01-07 02:36:45 +0100

    Add post method and cft validation.

--- BooksWebApp/Controllers/CategoryController.cs
+++ BooksWebApp/Controllers/CategoryController.cs
@@ -19,9 +19,46 @@ namespace BooksWebApp.Controllers
             return View(categoryList);
         }
 
+        // Get
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            try
+            {
+                if (category.Name == category.DisplayOrder.ToString())
+                {
+                    ModelState.AddModelError("OrderError", "The order can not match the Name.");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    var obj = _db.Categories.Single(c => c.Name == category.Name);
+                    if (obj == null)
+                    {
+                        _db.Categories.Add(category);
+                        _db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("CustomerError", "This category already.");
+                    }
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "An Expected Error!");
+            }
+
+            return View(category);
         }
     }
 }
