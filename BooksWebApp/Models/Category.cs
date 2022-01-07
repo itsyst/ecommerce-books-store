7c028401d5f669ab04cfb1827174191400b8cf87
Khaled El Hamzi <contact@elhamzi.me>
2022-01-07 13:01:50 +0100

    Apply custom validation annotation.

--- BooksWebApp/Models/Category.cs
+++ BooksWebApp/Models/Category.cs
@@ -10,6 +10,7 @@ namespace BooksWebApp.Models
         [Required]
         public string Name { get; set; }
         [Display(Name ="Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be between 1 and 100!")]
         public int DisplayOrder { get; set; }
         [Display(Name = "Created Date")]
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
