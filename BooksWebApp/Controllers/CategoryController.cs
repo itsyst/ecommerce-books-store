181d244f5ed3dd335af2d720a672690e128d5c1e
Khaled El Hamzi <contact@elhamzi.me>
2022-01-07 15:07:33 +0100

    Add delete & edit method to controller.

--- BooksWebApp/Controllers/CategoryController.cs
+++ BooksWebApp/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BooksWebApp.Data;
 using BooksWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BooksWebApp.Controllers
 {
@@ -59,6 +60,91 @@ namespace BooksWebApp.Controllers
             }
 
             return View(category);
+        }
+    
+        //Get
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //Post
+        // POST: Categories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DisplayOrder,CreatedDateTime")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(category);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _db.Categories.FindAsync(id);
+            _db.Categories.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _db.Categories.Any(c => c.Id == id);
         }
     }
 }
--- BooksWebApp/Models/Category.cs
+++ BooksWebApp/Models/Category.cs
@@ -9,10 +9,13 @@ namespace BooksWebApp.Models
 
         [Required]
         public string Name { get; set; }
+
         [Display(Name ="Order")]
-        [Range(1,100, ErrorMessage ="Display Order must be between 1 and 100!")]
+        [Range(1,100, ErrorMessage ="Display Order must be between 1 and 100 !")]
         public int DisplayOrder { get; set; }
+
         [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
     }
