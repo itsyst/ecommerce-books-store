73ac1961d1f19fa257dc1e602dd12ff72b5d4527
Khaled El Hamzi <contact@elhamzi.me>
2022-01-08 11:43:18 +0100

    Use development exception page.

--- BooksWebApp/Program.cs
+++ BooksWebApp/Program.cs
@@ -13,7 +13,11 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
