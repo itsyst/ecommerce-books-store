4780f5bf206463e10e56f0c06e28642c58331ff6
Khaled El Hamzi <contact@elhamzi.me>
2022-01-10 18:21:59 +0100

    Create  books cover model.

---
+++ Books.Domain/Entities/Cover.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Domain.Entities
+{
+    public class Cover
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(55)]
+        public string? Name { get; set; }    
+    }
+}
