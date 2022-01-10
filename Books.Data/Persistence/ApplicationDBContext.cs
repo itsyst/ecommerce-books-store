7bbfa8995193f8d36403c251381a07b446ac511b
Khaled El Hamzi <contact@elhamzi.me>
2022-01-10 02:39:17 +0100

    Move dbcontext to separate library.

--- Books.Data/Persistence/ApplicationDBContext.cs
+++ Books.Data/Persistence/ApplicationDBContext.cs
@@ -1,8 +1,9 @@
-﻿using Books.Models;
+﻿using Books.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Books.Data
+namespace Books.Data.Persistence
 {
+#pragma warning disable CS8618
     public class ApplicationDBContext:DbContext
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
