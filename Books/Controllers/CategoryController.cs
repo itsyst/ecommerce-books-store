3af6b23209c2e6d87879d2bfa7526951e41d2773
Khaled El Hamzi <contact@elhamzi.me>
2022-01-10 16:55:04 +0100

    Resolve the ambiguous match exception to avoid route conflicts.

--- Books/Controllers/CategoryController.cs
+++ Books/Controllers/CategoryController.cs
@@ -88,8 +88,8 @@ namespace Books.Controllers
         // POST: Employee/Edit/5  
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for   
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.  
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DisplayOrder,CreatedDateTime")] Category category)
         {
             if (id != category.Id)
