bd7f1e91c22690dc0f4ac47e1f8412d4e3ea49a7
Khaled El Hamzi <contact@elhamzi.me>
2022-01-10 16:36:21 +0100

    Create a unit of work interface.

---
+++ Books.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Books.Data.Persistence;
+using Books.Data.Repository;
+using Books.Interfaces;
+
+namespace Books.Data.UnitOfWork
+{
+
+# nullable disable
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private IGenericRepository<T> _entity;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> Entity
+        {
+            get
+            {
+                return _entity ??= new GenericRepository<T>(_context);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public async void SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Books.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Interfaces
+{
+    public interface IUnitOfWork<T> where T : class
+    {
+        IGenericRepository<T> Entity { get; }
+        void Save();
+         
+    }
+}
