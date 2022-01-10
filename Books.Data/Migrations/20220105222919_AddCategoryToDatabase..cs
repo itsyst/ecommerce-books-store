dc3ac3350238982720fb20e363094c08a8f12e24
Khaled El Hamzi <contact@elhamzi.me>
2022-01-10 02:40:44 +0100

    Move migrations to data access library.

--- Books.Data/Migrations/20220105222919_AddCategoryToDatabase..cs
+++ Books.Data/Migrations/20220105222919_AddCategoryToDatabase..cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace BooksWebApp.Migrations
+namespace Books.Data.Migrations
 {
     public partial class AddCategoryToDatabase : Migration
     {
