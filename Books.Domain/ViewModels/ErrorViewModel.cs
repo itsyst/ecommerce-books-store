d1ba29bf002abe3dcb0922e0b2145b54b9bbeacd
Khaled El Hamzi <contact@elhamzi.me>
2022-01-11 20:15:43 +0100

    Move error model to viewModels folder.

---
+++ Books.Domain/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Books.Domain.Entities
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
