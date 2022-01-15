dd9f23f4e096a2f8d613ad4b980a53c902431688
Khaled El Hamzi <contact@elhamzi.me>
2022-01-16 00:34:41 +0100

    Create product view model.

---
+++ Books.Domain/ViewModels/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Books.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Books.Domain.ViewModels
+{
+#pragma warning disable CS8618 
+
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+
+        [ValidateNever]
+        [Display(Name = "Author")]
+        public IEnumerable<SelectListItem> Authors { get; set; }
+
+        [ValidateNever]
+        [Display(Name = "Category")]
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        [ValidateNever]
+        [Display(Name = "Cover")]
+        public IEnumerable<SelectListItem> Covers { get; set; }
+
+    }
+}
