aed8d9b33e5373bebb58e79252d129f6fb27108c
Khaled El Hamzi <contact@elhamzi.me>
2022-01-16 12:50:03 +0100

    Add products api endpoint

---
+++ Books/Areas/Admin/Controllers/Api/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Books.Domain.Entities;
+using Books.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Areas.Admin.Controllers.Api
+{
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork<Product> _product;
+
+        public ProductController(IUnitOfWork<Product> product)
+        {
+            _product = product;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        [HttpHead]
+        // GET: / api / product
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _product.Entity.GetAllAsync();
+            //return Ok(products);
+            return Ok(products);
+        }
+    }
+}
