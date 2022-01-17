af2699ca9e293172af62b74366e57e0d61a5e737
Khaled El Hamzi <contact@elhamzi.me>
2022-01-17 22:47:36 +0100

    Update mapping profile.

--- Books/Profiles/MappingProfile.cs
+++ Books/Profiles/MappingProfile.cs
@@ -10,11 +10,23 @@ namespace Books.Profiles
         {
             // Domain to Dto
             CreateMap<Product, ProductDto>();
+            CreateMap<CategoryDto, CategoryDto>();
+            CreateMap<Cover, CoverDto>();
+            CreateMap<AuthorDto, AuthorDto>();
 
 
             // Dto to Domain
             CreateMap<ProductDto, Product>()
                 .ForMember(p => p.Id, opt => opt.Ignore());
+
+            CreateMap<CategoryDto, Product>()
+                 .ForMember(p => p.Id, opt => opt.Ignore());
+
+            CreateMap<CoverDto, Product>()
+                .ForMember(p => p.Id, opt => opt.Ignore());
+
+            CreateMap<AuthorDto, Product>()
+                 .ForMember(p => p.Id, opt => opt.Ignore());
         }
     }
 }
