d92a1e1fcfa37cd8e53a462c2e1821897e52af09
Khaled El Hamzi <contact@elhamzi.me>
2022-01-19 00:47:16 +0100

    Create shopping cart model view.

---
+++ Books.Domain/ViewModels/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using Books.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Books.Domain.ViewModels
+{
+    public class ShoppingCart
+    {
+        public Product? Product { get; set; }
+
+        [Range(1,1000, ErrorMessage="Please enter a valur between 1 and 1000")]
+        public int Count { get; set; }
+    }
+}
