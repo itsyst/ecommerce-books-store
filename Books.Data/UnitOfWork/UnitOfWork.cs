507af6f5c3eb235e2720b9e45a15b7168a34364c
Khaled El Hamzi <contact@elhamzi.me>
2022-01-18 21:48:16 +0100

    Implement a dispose method  to release unmanaged resources.

--- Books.Data/UnitOfWork/UnitOfWork.cs
+++ Books.Data/UnitOfWork/UnitOfWork.cs
@@ -7,10 +7,11 @@ namespace Books.Data.UnitOfWork
 {
 
 # nullable disable
-    public class UnitOfWork<T> : IUnitOfWork<T> where T : class 
+    public class UnitOfWork<T> : IDisposable, IUnitOfWork<T> where T : class 
     {
         private readonly ApplicationDbContext _context;
         public IGenericRepository<T> _entity;
+        private bool _disposed;
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
@@ -37,6 +38,22 @@ namespace Books.Data.UnitOfWork
             return true;
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
