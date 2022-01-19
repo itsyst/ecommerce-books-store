15e5bd393cd2cbefa3ffe91c5287508488163b6c
Khaled El Hamzi <contact@elhamzi.me>
2022-01-19 20:51:25 +0100

    Add update and get action methods to company controller.

--- Books/Areas/Admin/Controllers/Api/CompaniesController.cs
+++ Books/Areas/Admin/Controllers/Api/CompaniesController.cs
@@ -1,4 +1,6 @@
-﻿using Books.Domain.Entities;
+﻿using AutoMapper;
+using Books.Domain.Entities;
+using Books.Dtos;
 using Books.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,19 +13,74 @@ namespace Books.Areas.Admin.Controllers.Api
     public class CompaniesController : ControllerBase
     {
         private readonly IUnitOfWork<Company> _company;
- 
-        public CompaniesController(IUnitOfWork<Company> company)
+        private readonly IMapper _mapper;
+
+        public CompaniesController(IUnitOfWork<Company> company, IMapper mapper)
         {
             _company = company;
+            _mapper = mapper;
         }
 
-        // GET: / api / companies
+        // GET: /api/companies
         [HttpGet]
         [HttpHead]
         public async Task<IActionResult> GetCompanies()
         {
             var companies = await _company.Entity.GetAllAsync();
             return Ok(companies);
+        }
+
+        //GET: /api/v1/companies/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+
+            var company = await _company.Entity.GetFirstOrDefaultAsync(p => p.Id == id);
+
+            if (company == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<Company, CompanyDto>(company));
+        }
+
+
+        // PUT /api/products/1
+        [HttpPut]
+        public async Task<IActionResult> Upsert(int id, CompanyDto companyDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var companyInDb = await _company.Entity.GetFirstOrDefaultAsync(p => p.Id == id);
+
+            if (companyInDb == null)
+                return NotFound();
+
+            var _mappedCompany = _mapper.Map(companyDto, companyInDb);
+
+            await _company.Entity.UpdateAsync(_mappedCompany);
+            await _company.SaveAsync();
+            await _company.CompleteAsync();
+
+            return Ok();
+        }
+
+
+        // GET: /api/companies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            Company companyInDb = await _company.Entity.GetFirstOrDefaultAsync(c => c.Id == id);
+            
+            if (companyInDb == null)
+                return NotFound();
+
+            await _company.Entity.DeleteAsync(companyInDb.Id);
+            await _company.SaveAsync();
+            await _company.CompleteAsync();
+
+            return Ok(companyInDb);
+
         }
     }
 }
