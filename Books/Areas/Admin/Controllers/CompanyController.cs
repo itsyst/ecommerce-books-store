66e60c42d8a7a519adc575232ca41d162226c56c
Khaled El Hamzi <contact@elhamzi.me>
2022-01-19 22:36:36 +0100

    Add upsert method to company controller.

--- Books/Areas/Admin/Controllers/CompanyController.cs
+++ Books/Areas/Admin/Controllers/CompanyController.cs
@@ -1,4 +1,5 @@
 ﻿using Books.Domain.Entities;
+using Books.Dtos;
 using Books.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,11 +15,53 @@ namespace Books.Areas.Admin.Controllers
             _company = company;
         }
 
+        
+
         // GET: CompanyController
         public async Task<IActionResult> Index()
         {
             IEnumerable<Company> companies = await _company.Entity.GetAllAsync();
             return View(companies);
+        }
+
+        // GET: ProductController/Upsert/5
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Company company = new();
+
+            if (id == null || id == 0)
+                return View(company);
+            else
+            {
+                company = await _company.Entity.GetFirstOrDefaultAsync(u => u.Id == id);
+                return View(company);
+            }
+        }
+
+        // POST: ProductController/Upsert/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+ 
+
+                if (company.Id == 0)
+                    await _company.Entity.InsertAsync(company);
+
+                else
+                {
+                    await _company.Entity.UpdateAsync(company);
+                }
+
+                await _company.SaveAsync();
+                await _company.CompleteAsync();
+                TempData["Success"] = "Company created successfully.";
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
         }
     }
 }
