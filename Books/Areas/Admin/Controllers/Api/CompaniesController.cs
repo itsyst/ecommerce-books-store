30b25ce157f23eb7b7a8ec0bb29a59768848f035
Khaled El Hamzi <contact@elhamzi.me>
2022-01-19 20:13:44 +0100

    Create api controller - fech data method.

---
+++ Books/Areas/Admin/Controllers/Api/CompaniesController.cs
@@ -0,0 +1,29 @@
+﻿using Books.Domain.Entities;
+using Books.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Areas.Admin.Controllers.Api
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly IUnitOfWork<Company> _company;
+ 
+        public CompaniesController(IUnitOfWork<Company> company)
+        {
+            _company = company;
+        }
+
+        // GET: / api / companies
+        [HttpGet]
+        [HttpHead]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _company.Entity.GetAllAsync();
+            return Ok(companies);
+        }
+    }
+}
