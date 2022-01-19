bafac342527ea16fa5f67ff7721ce3d1bd9592e2
Khaled El Hamzi <contact@elhamzi.me>
2022-01-19 18:48:53 +0100

    Create company model.

---
+++ Books.Domain/Entities/Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
