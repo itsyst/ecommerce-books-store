e809f2a32d493c50635063f370884e5e795119b4
Khaled El Hamzi <contact@elhamzi.me>
2022-01-19 20:14:44 +0100

    Create company controller and add first crud opperation.

---
+++ Books/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using Books.Domain.Entities;
+using Books.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork<Company> _company;
+
+        public CompanyController(IUnitOfWork<Company> company)
+        {
+            _company = company;
+        }
+
+        // GET: CompanyController
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Company> companies = await _company.Entity.GetAllAsync();
+            return View(companies);
+        }
+    }
+}
