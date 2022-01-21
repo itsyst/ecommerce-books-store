439c4a1cd00a8df760bccee17df00c4401cf32d4
Khaled El Hamzi <contact@elhamzi.me>
2022-01-21 22:23:35 +0100

    Add mapping profile useful for automapper.

---
+++ Books/Dtos/ShoppingCartDto.cs
@@ -0,0 +1,30 @@
+﻿using Books.Domain.Entities;
+using Books.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Books.Dtos
+{
+    public class ShoppingCartDto
+    {
+        public Guid Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product? Product { get; set; } 
+
+
+        [Range(1, 1000, ErrorMessage = "Please enter a valur between 1 and 1000")]
+        public int Count { get; set; }
+
+        public string? ApplicationUserId { get; set; }
+
+
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser? ApplicationUser { get; set; }
+    }
+}
