68c00a2eaa7010f950d72a81de8dfbb516912b39
Khaled El Hamzi <contact@elhamzi.me>
2022-01-23 22:10:29 +0100

    Retrieve shopping carts by customer.

--- Books/Areas/Customer/Controllers/CartController.cs
+++ Books/Areas/Customer/Controllers/CartController.cs
@@ -1,14 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Books.Domain.Entities;
+using Books.Domain.ViewModels;
+using Books.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Books.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork<ShoppingCart> _shoppingCart;
+        public CartController(IUnitOfWork<ShoppingCart> shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
         // GET: CartController
         public async Task<ActionResult> Index()
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var cart = new ShoppingCartViewModel()
+            {
+                ShoppingCarts = await _shoppingCart.Entity.GetAllAsync(
+                    c => c.ApplicationUserId == userId,
+                   includeProperties: "Product"),
+
+
+            }; 
+            return View(cart);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _shoppingCart.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
