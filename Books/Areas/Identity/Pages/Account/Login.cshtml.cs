f0747e903f955bf9113cca9e48a4be0490835876
Khaled El Hamzi <contact@elhamzi.me>
2022-01-23 14:47:30 +0100

    Simplify member name.

--- Books/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Books/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -112,7 +112,7 @@ namespace Books.Areas.Identity.Pages.Account
                 }
                 if (result.RequiresTwoFactor)
                 {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, Input.RememberMe });
                 }
                 if (result.IsLockedOut)
                 {
