8169fccef181f863018005b80030d0a6e36aa751
Khaled El Hamzi <contact@elhamzi.me>
2022-01-24 13:30:40 +0100

    Add necessary actions for adding , substructing, and removing products from basket.

--- Books/Areas/Customer/Controllers/CartController.cs
+++ Books/Areas/Customer/Controllers/CartController.cs
@@ -12,14 +12,16 @@ namespace Books.Areas.Customer.Controllers
     [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork<Product> _product;
         private readonly IUnitOfWork<ShoppingCart> _shoppingCart;
 
-        public CartController(IUnitOfWork<ShoppingCart> shoppingCart)
+        public CartController(IUnitOfWork<Product> product, IUnitOfWork<ShoppingCart> shoppingCart)
         {
+            _product = product;
             _shoppingCart = shoppingCart;
         }
         // GET: CartController
-        public async Task<ActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
@@ -41,5 +43,88 @@ namespace Books.Areas.Customer.Controllers
             return View(cart);
         }
 
+        /// <summary>
+        /// Add new item to the shopping basket.
+        /// </summary>
+        /// <param name="cartId">Shopping cart Id</param>
+        /// <returns></returns>
+        public async Task<IActionResult> AddItem(Guid? cartId)
+        {
+            ShoppingCart? cart = await _shoppingCart.Entity.GetFirstOrDefaultAsync(u => u.Id == cartId, includeProperties: "Product");
+
+            if(cart != null)
+            {
+                if (cart.Product.InStock > 0)
+                {
+                    cart.Count += 1;
+                    await _shoppingCart.Entity.UpdateAsync(cart);
+                    await _shoppingCart.CompleteAsync();
+
+                    cart.Product.InStock -= 1;
+                    await _product.Entity.UpdateAsync(cart.Product);
+                    await _product.CompleteAsync();
+                }
+                else
+                {
+                    TempData["Error"] = "Not enough items In stock.";
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        /// <summary>
+        /// Remove items from the shopping basket.
+        /// </summary>
+        /// <param name="cartId">Shopping cart Id</param>
+        /// <returns></returns>
+        public async Task<IActionResult> RemoveItem(Guid? cartId)
+        {
+            ShoppingCart? cart = await _shoppingCart.Entity.GetFirstOrDefaultAsync(u => u.Id == cartId, includeProperties: "Product");
+
+            if (cart != null)
+            {
+                if (cart.Count > 0)
+                {
+                    cart.Count -= 1;
+                    await _shoppingCart.Entity.UpdateAsync(cart);
+                    await _shoppingCart.CompleteAsync();
+
+                    cart.Product.InStock += 1;
+                    await _product.Entity.UpdateAsync(cart.Product);
+                    await _product.CompleteAsync();
+                }
+
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        /// <summary>
+        /// Remove item from the shopping cart.
+        /// </summary>
+        /// <param name="cartId"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> RemoveFromBasket(Guid cartId)
+        {
+            ShoppingCart? cart = await _shoppingCart.Entity.GetFirstOrDefaultAsync(u => u.Id == cartId, includeProperties: "Product");
+            var items = cart.Count;
+
+            if (cart != null)
+            {
+                cart.Count -= items;
+                await _shoppingCart.Entity.UpdateAsync(cart);
+                await _shoppingCart.CompleteAsync();
+
+                cart.Product.InStock += items;
+                await _product.Entity.UpdateAsync(cart.Product);
+                await _product.CompleteAsync();
+            }
+
+            await _shoppingCart.Entity.DeleteAsync(cartId);
+            await _shoppingCart.CompleteAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
