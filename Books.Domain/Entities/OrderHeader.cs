6b2ba0972a2cb2d1262d3ca43d7ca1d17873e894
Khaled El Hamzi <contact@elhamzi.me>
2022-01-24 19:52:49 +0100

    Create shipping order header model.

---
+++ Books.Domain/Entities/OrderHeader.cs
@@ -0,0 +1,54 @@
+﻿using Books.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Books.Domain.Entities
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? ApplicationUserId { get; set; }
+
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        [Required]
+        [MaxLength(55)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(55)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Street Adress")]
+        public string? StreetAddress { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+
+        [Required]
+        public string? State { get; set; }
+
+        [Required]
+        [Display(Name = "Postal Code")]
+        [DataType(DataType.PostalCode)]
+        public string? PostalCode { get; set; }
+
+        [Display(Name = "Company Name")]
+        public Company? Company { get; set; } = null;
+
+        public double OrderTotal { get; set; } = 0;
+    }
+}
