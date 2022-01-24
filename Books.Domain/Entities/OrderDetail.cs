eb396268d25da77899f25cca2868c214bc60353e
Khaled El Hamzi <contact@elhamzi.me>
2022-01-24 19:52:05 +0100

    Create order details model.

---
+++ Books.Domain/Entities/OrderDetail.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Books.Domain.Entities
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        [ValidateNever]
+        public OrderHeader? OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product? Product { get; set; }
+
+        public int Count { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
