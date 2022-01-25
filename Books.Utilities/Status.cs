d1f6f604ac333a994c744063a51c4456039f6e0e
Khaled El Hamzi <contact@elhamzi.me>
2022-01-25 01:47:22 +0100

    Create constant status utilities.

---
+++ Books.Utilities/Status.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Books.Utilities
+{
+    public static class Status
+    {
+        public enum StatusType
+        {
+            Pending,
+            Approved,
+            InProgress,
+            Shipped,
+            Canceled,
+            Refunded
+        }
+
+        public enum Payment
+        {
+            Pending,
+            Approved,
+            Delayed,
+            Rejected
+        }
+    }
+}
