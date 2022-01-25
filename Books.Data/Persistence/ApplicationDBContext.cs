d6461560e498837d1afc284278673b2303997135
Khaled El Hamzi <contact@elhamzi.me>
2022-01-25 01:50:06 +0100

    Add new entries to dbcontext.

--- Books.Data/Persistence/ApplicationDBContext.cs
+++ Books.Data/Persistence/ApplicationDBContext.cs
@@ -15,6 +15,8 @@ namespace Books.Data.Persistence
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
