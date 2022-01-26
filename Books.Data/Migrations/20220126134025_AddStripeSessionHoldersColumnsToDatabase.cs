9f592047f7d38c8c0291742ba330ae3efab13438
Khaled El Hamzi <contact@elhamzi.me>
2022-01-26 23:51:06 +0100

    Add stripe session id columns to database.

---
+++ Books.Data/Migrations/20220126134025_AddStripeSessionHoldersColumnsToDatabase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Books.Data.Migrations
+{
+    public partial class AddStripeSessionHoldersColumnsToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderDate",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "OrderHeader");
+
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "OrderHeader");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderDate",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
