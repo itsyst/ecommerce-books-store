9966031edc77e0f1d7360c0c83de29a05ddf08cc
Khaled El Hamzi <contact@elhamzi.me>
2022-01-30 22:55:28 +0100

    Update order header table.

---
+++ Books.Data/Migrations/20220130212520_AddNewRoleToRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Books.Data.Migrations
+{
+    public partial class AddNewRoleToRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentStatus",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentStatus",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
