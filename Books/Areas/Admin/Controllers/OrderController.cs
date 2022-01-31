d2a25cfce8b3c438cba9a25f6ffd4483ad01da23
Khaled El Hamzi <contact@elhamzi.me>
2022-01-31 20:58:19 +0100

    Create action method details.

--- Books/Areas/Admin/Controllers/OrderController.cs
+++ Books/Areas/Admin/Controllers/OrderController.cs
@@ -1,21 +1,31 @@
 ﻿using Books.Domain.Entities;
+using Books.Domain.ViewModels;
 using Books.Interfaces;
+using Books.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Books.Areas.Admin.Controllers
 {
+#pragma warning disable
+
     [Area("Admin")]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork<OrderHeader> _orderHeader;
         private readonly IUnitOfWork<OrderDetail> _orderDetail;
+        private readonly IUnitOfWork<ApplicationUser> _applicationUser;
+
+        [BindProperty]
+        public OrderViewModel OrderViewModel { get; set; }
 
         public OrderController(
             IUnitOfWork<OrderHeader> orderHeader,
-            IUnitOfWork<OrderDetail> orderDetail)
+            IUnitOfWork<OrderDetail> orderDetail,
+            IUnitOfWork<ApplicationUser> applicationUser)
         {
             _orderHeader = orderHeader;
             _orderDetail = orderDetail;
+            _applicationUser = applicationUser;
         }
 
         // GET: Order
@@ -23,6 +33,28 @@ namespace Books.Areas.Admin.Controllers
         {
             await Task.CompletedTask;
             return View();
+        }
+
+        // GET: Order/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderViewModel = new()
+            {
+                OrderHeader = await _orderHeader.Entity.GetFirstOrDefaultAsync(o => o.Id == id, includeProperties: "ApplicationUser"),
+                OrderDetail = await _orderDetail.Entity.GetAllAsync(filter: o => o.Id == id, p => p.Product, h => h.OrderHeader),
+            };
+
+ 
+            if (OrderViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(OrderViewModel);
         }
     }
 }
