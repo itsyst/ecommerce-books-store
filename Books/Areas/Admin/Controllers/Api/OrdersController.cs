95b1dd14f0ff67e27a5e0f9b0cb6bff65b78d05f
Khaled El Hamzi <contact@elhamzi.me>
2022-01-31 01:28:42 +0100

    Create order api controller.

---
+++ Books/Areas/Admin/Controllers/Api/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Books.Domain.Entities;
+using Books.Interfaces;
+using Books.Models;
+using Books.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Books.Areas.Admin.Controllers.Api
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IUnitOfWork<OrderHeader> _orderHeader;
+        private readonly IUnitOfWork<ApplicationUser> _applicationUser;
+        private readonly IMapper _mapper;
+
+        public OrdersController(
+            IUnitOfWork<OrderHeader> orderHeader,
+            IUnitOfWork<ApplicationUser> applicationUser,
+            IMapper mapper)
+        {
+            _applicationUser = applicationUser;
+            _orderHeader = orderHeader;
+            _mapper = mapper;
+        }
+
+        // GET: /api/orders
+        [HttpGet]
+        [HttpHead]
+        public async Task<IActionResult> GetOrders()
+        {
+            IEnumerable<OrderHeader> orderHeaderInDB;
+            if (User.IsInRole(Roles.RoleType.Admin.ToString()) || User.IsInRole(Roles.RoleType.Employee.ToString()))
+                orderHeaderInDB = await _orderHeader.Entity.GetAllAsync(includeProperties: "ApplicationUser");
+            else
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                orderHeaderInDB = await _orderHeader.Entity.GetAllAsync(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+
+            }
+            return Ok(orderHeaderInDB);
+        }
+    }
+}
