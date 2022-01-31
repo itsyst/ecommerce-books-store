23344d52ff6eb0c98cdd78b05903cbd2eedd9888
Khaled El Hamzi <contact@elhamzi.me>
2022-01-31 01:28:57 +0100

    create order controller.

---
+++ Books/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Books.Domain.Entities;
+using Books.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork<OrderHeader> _orderHeader;
+        private readonly IUnitOfWork<OrderDetail> _orderDetail;
+
+        public OrderController(
+            IUnitOfWork<OrderHeader> orderHeader,
+            IUnitOfWork<OrderDetail> orderDetail)
+        {
+            _orderHeader = orderHeader;
+            _orderDetail = orderDetail;
+        }
+
+        // GET: Order
+        public async Task<IActionResult> Index()
+        {
+            await Task.CompletedTask;
+            return View();
+        }
+    }
+}
