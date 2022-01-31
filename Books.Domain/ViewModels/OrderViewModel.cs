0b794e9b863c028d766081575318fe14b796d973
Khaled El Hamzi <contact@elhamzi.me>
2022-01-31 01:30:35 +0100

    Create order view model.

---
+++ Books.Domain/ViewModels/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Books.Domain.Entities;
+
+namespace Books.Domain.ViewModels
+{
+    public class OrderViewModel
+    {
+        public OrderHeader OrderHeader { get; set; } = new();
+        public IEnumerable<OrderDetail>? OrderDetail { get; set; }
+    }
+}
